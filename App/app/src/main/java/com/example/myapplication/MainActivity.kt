8fc76bf2202642aa3e182d7d4adadbdaa27b6f31
wingding01 <wenbin.xie@tamu.edu>
2024-09-28 22:30:48 -0500

    Added Reset Timing

--- App/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ App/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -12,6 +14,9 @@ class MainActivity : AppCompatActivity() {
 
     // Initialize a counter variable
     private var counter = 0
+
+    // Initialize a boolean variable to track if the counter is resetting
+    private var isResetting = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,18 +30,34 @@ class MainActivity : AppCompatActivity() {
 
 
         // Find the button by its ID and set its click listener
+
         var button: View = findViewById(R.id.button)
+        var reset: View = findViewById(R.id.reset)
 
         // Find the TextView by its ID and set its text
         var count: TextView = findViewById(R.id.count)
 
         // Set a click listener on the button
         button.setOnClickListener {
-            // Increment the counter and update the TextView
-            counter++
-            count.setText("You clicked me $counter times")
+            // Increment the counter and update the TextView if reset is not being pressed
+            Handler(Looper.getMainLooper()).postDelayed({
+                if(!isResetting) {
+                counter++
+                count.setText("You clicked me $counter times")
+                }
+            }, 50)
         }
 
+        reset.setOnClickListener {
+            isResetting = true
+            counter = 0
+            count.setText("Click Below")
+
+            // Introduce a short delay to prevent double button press
+            Handler(Looper.getMainLooper()).postDelayed({
+                isResetting = false
+            }, 500)
+        }
 
 
     }
