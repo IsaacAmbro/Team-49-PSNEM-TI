7b12dcb50c6f4fc8c11630c49d1f70decd23dab2
wingding01 <wenbin.xie@tamu.edu>
2024-10-07 19:59:05 -0500

    New Folder for actual app

--- App Dev/app/src/main/java/com/example/appdev/pairedList.kt
+++ App Dev/app/src/main/java/com/example/appdev/pairedList.kt
@@ -1,29 +1,29 @@
 package com.example.appdev
 
-import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class pairedList : AppCompatActivity() {
 
     private var mBluetoothAdapter: BluetoothAdapter? = null
     private val REQUEST_ENABLE_BLUETOOTH = 1
+    lateinit var mPairedDevices: Set<BluetoothDevice>
+
+
 
     val registerForResult = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
@@ -35,33 +35,58 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
+    companion object {
+        val EXTRA_ADDRESS: String = "Device_address"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_paired_list)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val switchList = findViewById<Button>(R.id.switchList)
-        switchList.setOnClickListener {
-            val intent = Intent(this, pairedList::class.java)
-            startActivity(intent)
-        }
-
 
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
 
-        // Check if bluetooth enabled
         if (mBluetoothAdapter?.isEnabled == false) {
             val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             registerForResult.launch(enableBtIntent)
         }
 
+        val refresh = findViewById<Button>(R.id.refresh)
+        refresh.setOnClickListener { pairedDeviceList() }
 
+
+    }
+
+
+    // Get the already paired devices
+    @SuppressLint("MissingPermission")
+    private fun pairedDeviceList(){
+        mPairedDevices = mBluetoothAdapter!!.bondedDevices
+        val list : ArrayList<BluetoothDevice> = ArrayList()
+
+        if(!mPairedDevices.isEmpty()){
+            for(device: BluetoothDevice in mPairedDevices){
+                list.add(device)
+            }
+        }
+        else{
+            Toast.makeText(this, "No Paired Devices", Toast.LENGTH_LONG).show()
+        }
+
+        val adapater = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+
+        val pairedList = findViewById<ListView>(R.id.pairedList)
+        pairedList.adapter = adapater
+        pairedList.onItemClickListener = AdapterView.OnItemClickListener{ _,_,position, _ ->
+            val device : BluetoothDevice = list[position]
+            val address: String = device.address
+
+        }
     }
 }
